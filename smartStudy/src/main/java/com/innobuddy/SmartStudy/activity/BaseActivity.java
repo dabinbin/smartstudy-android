ebbc237c086599e63c36a0c23f7f9ec7246a5c9d
zhengmingbin <zhengmingbin@innobuddy.com>
2015-05-12 19:47:49 +0800

    第2次

--- smartStudy/src/main/java/com/innobuddy/SmartStudy/activity/BaseActivity.java
+++ smartStudy/src/main/java/com/innobuddy/SmartStudy/activity/BaseActivity.java
@@ -24,6 +24,7 @@ public class BaseActivity extends Activity implements OnClickListener {
 		
 //		123
 		//2222
+		//22
 		/******************/
 		http=new HttpUtils();
 		http.configCurrentHttpCacheExpiry(0); // 请求缓存
